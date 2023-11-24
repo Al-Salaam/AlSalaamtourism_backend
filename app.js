90ea85f8aa04d95b62fb64c062fbf31ca320c1fd
Luqman-Khokhar <muhammadluqmang@gmail.com>
2023-11-24 13:29:26 +0500

    Admin Login Bug Fix

--- app.js
+++ app.js
@@ -20,6 +20,14 @@ const app = express();
 
 dotenv.config();
 
+
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 const allowedOrigins = [
   "http://localhost:3000",
   "https://al-salaam-tourism-c93fa215ea59.herokuapp.com/",
